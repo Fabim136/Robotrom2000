e5f5a944f98a557c6c1db36977c25c97addd661d
Fabiano Silva <fabianojunior136@gmail.com>
2023-08-14 07:40:37 -0300

    código refatorado

--- js/main.js
+++ js/main.js
@@ -6,23 +6,14 @@ const controle = document.querySelectorAll(".controle-ajuste")
 
 controle.forEach ( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        console.log(evento.target)
+        manipulaDados(evento.target.textContent)
     })
 })
 
-
-somar.addEventListener("click", () => {manipulaDados("somar") })
-
-subtrair.addEventListener('click', () => {manipulaDados("subtrair") })
-
-
-
-
-
 function manipulaDados (operacao) {
-    if (operacao === 'subtrair') {
+    if (operacao === '-') {
         braco.value = parseInt(braco.value) -1
-    } else if (operacao === 'somar') {
+    } else if (operacao === '+') {
         braco.value = parseInt (braco.value) +1
     }
 }
